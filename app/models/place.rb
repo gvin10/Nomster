a963cd5f20e9215de487b833a5324da7558c58b5
Gregorio Vincent <gv46@cornell.edu>
2017-10-28 20:50:24 +0000

    lesson 29

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   validates :name, :description, :address, presence: true
   validates :name, :description, :address, length: {minimum: 3}
 end
