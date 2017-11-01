f12008393207f8cd2db477a5574d22b27735c745
Gregorio Vincent <gv46@cornell.edu>
2017-11-01 04:07:38 +0000

    photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :place
   belongs_to :user
   mount_uploader :picture, PictureUploader
+  validates :caption, :picture, presence: true
+  validates :caption, length: {minimum: 3}
 end
